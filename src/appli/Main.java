717fd2ebd4096a169dd2869c7a563c9bf0dbca17
Ayata BayramEfe <bayram-efe.ayata4@etu.univ-lorraine.fr>
2025-05-09 22:03:59 +0200

    Début de structure graphique

--- src/appli/Main.java
+++ src/appli/Main.java
@@ -1,3 +1,5 @@
+package appli;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -6,13 +8,17 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public  class Main extends Application {
-
+    public static void main(String[] args) {
+        launch(args);
+    }
     @Override
     public void start(Stage primaryStage) throws Exception{
-        BorderPane root = new BorderPane();
-        primaryStage.setTitle("My First JavaFX Application");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vue/Vue_Globale.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Collection Joueur de Football");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
 
     }
+
 }
---
+++ src/appli/controleurs/VueGlobaleController.java
@@ -0,0 +1,10 @@
+package appli.controleurs;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+
+public class VueGlobaleController {
+    @FXML
+    ScrollPane scrollPane;
+
+}
