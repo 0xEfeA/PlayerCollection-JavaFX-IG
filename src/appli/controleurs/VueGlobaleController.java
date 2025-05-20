c80b35b683108fa3cba810bb7e1f8e048582bee1
GoatEfe <efeayata88@icloud.com>
2025-05-20 11:30:21 +0200

    Ajustement graphique

--- src/appli/controleurs/VueGlobaleController.java
+++ src/appli/controleurs/VueGlobaleController.java
@@ -1,10 +1,28 @@
 package appli.controleurs;
 
+import appli.modele.Joueur;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+
+import javax.swing.text.html.ImageView;
 
 public class VueGlobaleController {
     @FXML
-    ScrollPane scrollPane;
+    private ListView<Joueur> listViewJoueur;
+    @FXML
+    private Button boutonAjouter;
+    @FXML
+    private Button boutonEdition;
+    @FXML
+    private Button boutonTrier;
+    @FXML
+    private ImageView iconeRecherche;
+    @FXML
+    private TextField rechercheText;
+    public VueGlobaleController(){}
 
+    }
 }
